414bf35bb198cbc8833877f35c38178f2a040fef
sclam <zvmkm@mail.ru>
2022-06-12 20:49:09 +0300

    add_multiconnection_test

--- Server/src/test/java/org/example/AppTest.java
+++ Server/src/test/java/org/example/AppTest.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.example.config.ServerApplicationConfig;
 import org.example.server.Server;
@@ -11,9 +12,9 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
-import java.io.File;
 import java.io.IOException;
 import java.net.Socket;
+import java.util.concurrent.*;
 
 public class AppTest {
     private static Thread serverThread;
@@ -194,6 +195,87 @@ public class AppTest {
                 "  \"response\": \"ERROR\",\n" +
                 "  \"reason\": \"No such key\"\n" +
                 "}", received);
+    }
+
+    @Test
+    public void testMultiConnection() {
+        ConcurrentLinkedDeque<String> expected = new ConcurrentLinkedDeque<>();
+        ConcurrentLinkedDeque<String> actual = new ConcurrentLinkedDeque<>();
+        expected.add("{\n" +
+                "  \"response\": \"OK\"\n" +
+                "}");
+        expected.add("{\n" +
+                "  \"response\": \"OK\",\n" +
+                "  \"value\": \"Elon Musk\"\n" +
+                "}");
+        expected.add("{\n" +
+                "  \"response\": \"ERROR\",\n" +
+                "  \"reason\": \"No such key\"\n" +
+                "}");
+        expected.add("{\n" +
+                "  \"response\": \"OK\",\n" +
+                "  \"value\": \"Hello world!\"\n" +
+                "}");
+        ExecutorService executor = Executors.newFixedThreadPool(4);
+        executor.submit(() -> {
+            String received = null;
+            try (Socket socket = new Socket("127.0.0.1", port);
+                 DataOutputStream output = new DataOutputStream(socket.getOutputStream());
+                 DataInputStream input = new DataInputStream(socket.getInputStream())) {
+                output.writeUTF("{\"type\":\"set\",\"key\":\"person\",\"value\":{\"name\":\"Elon Musk\",\"car\":{\"model\":\"Tesla Roadster\",\"year\":\"2018\"},\"rocket\":{\"name\":\"Falcon 9\", \"launches\":\"87\"}}}");
+                received = input.readUTF();
+                actual.add(received);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+        executor.submit(() -> {
+            String received = null;
+            try (Socket socket = new Socket("127.0.0.1", port);
+                 DataOutputStream output = new DataOutputStream(socket.getOutputStream());
+                 DataInputStream input = new DataInputStream(socket.getInputStream())) {
+                output.writeUTF("{\"type\":\"get\",\"key\":[\"person\",\"name\"]}");
+                received = input.readUTF();
+                actual.add(received);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+        executor.submit(() -> {
+            String received = null;
+            try (Socket socket = new Socket("127.0.0.1", port);
+                 DataOutputStream output = new DataOutputStream(socket.getOutputStream());
+                 DataInputStream input = new DataInputStream(socket.getInputStream())) {
+                output.writeUTF("{\"type\":\"get\",\"key\":[\"person\",\"nameTEST\"]}");
+                received = input.readUTF();
+                actual.add(received);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+        executor.submit(() -> {
+            String received = null;
+            try (Socket socket = new Socket("127.0.0.1", port);
+                 DataOutputStream output = new DataOutputStream(socket.getOutputStream());
+                 DataInputStream input = new DataInputStream(socket.getInputStream())) {
+                output.writeUTF("{\"type\":\"get\", \"key\":\"1\"}");
+                received = input.readUTF();
+                actual.add(received);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+        executor.shutdown();
+        try {
+            if (!executor.awaitTermination(500, TimeUnit.MILLISECONDS)) {
+                System.out.println("Executor still running");
+            }
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        assertTrue(expected.size() == actual.size()
+                && expected.containsAll(actual)
+                && actual.containsAll(expected));
     }
 
     @AfterAll
